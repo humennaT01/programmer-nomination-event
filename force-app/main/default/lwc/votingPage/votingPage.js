0a987a7ab901a7694ab12a8507465f582338a2e2
Tetiana Humenna <tetiana.humenna12@gmail.com>
2021-01-07 20:31:33 +0200

    Updated voting page

--- force-app/main/default/lwc/votingPage/votingPage.js
+++ force-app/main/default/lwc/votingPage/votingPage.js
@@ -1,22 +1,22 @@
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track} from 'lwc';
 import getCampaignList from '@salesforce/apex/VotingController.getCampaignList';
 import getNominationList from '@salesforce/apex/VotingController.getNominationList';
 import createVote from '@salesforce/apex/VotingController.createVote';
 import getContactList from '@salesforce/apex/VotingController.getContactList';
-// import getVoter from '@salesforce/apex/VotingController.getVoter';
+import getDescription from '@salesforce/apex/VotingController.getDescription';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class VotingPage extends LightningElement {
+export default class VotingPage extends NavigationMixin(LightningElement) {
     queryTerm; 
     contacts;
-    contactsInNominations;
-    nominations;
+    @track contactsInNominations;
+    @track nominations;
     campaign;
     voterEmail;
     selectedNomination;
     selectedCampaign;
     posibleVotes = {};
-    updateContacts = false;
     hasVoted = false;
     connectedCallback(){
         getCampaignList().then(result => { 
@@ -26,46 +26,24 @@ export default class VotingPage extends LightningElement {
                 getContactList({nominations: this.nominations}).then(result => { 
                     this.contactsInNominations = result;
                     this.nominations.forEach(nominationItem => {
+                        this.contactsInNominations[nominationItem.Id].forEach(contactItem => {
+                            getDescription({contactId: contactItem.Id, nominationId: nominationItem.Id}).then(result => {
+                                contactItem.description =  result;
+                            });            
+                        });
                         nominationItem.contacts = this.contactsInNominations[nominationItem.Id];
                     });
-                    this.updateContacts = true;
                 });
             });
 
         });
         
     }
-
-
-    // @wire(getContactList, {queryTerm:'$queryTerm', nominationsId: '$selectedNomination'})
-    // wiredContacts(response){
-    //     this.contacts = response.data;
-    // }
-    // @wire(getNominationList, {campaign: '$selectedCampaign'})
-    // wiredNominations(response){
-    //     this.nominations = response.data;        
-    // }
-    // @wire(getCampaignList)
-    // wiredCampaigns(response){
-    //     this.campaigns = response.data;
-    // }
     handleEmailChange(evt){
         this.voterEmail = evt.target.value;
     }
     
-    // handleSearchChange(evt) {
-    //     this.queryTerm = evt.target.value;
-    // }
-
-    // handleNominationChange(event) {
-    //     this.selectedNomination = event.detail.value;
-    // }
-    
-    // handleCampaignChange(event) {
-    //     this.selectedCampaign = event.detail.value;
-    // }
-
-    handleClickButton() {
+    handleClickButton(evt) {
         if(!this.voterEmail){
             this.dispatchEvent(
                 new ShowToastEvent({
@@ -75,23 +53,19 @@ export default class VotingPage extends LightningElement {
                 })
             );
         }else{
-        // let voterContactId = getVoter({email: this.voterEmail});
-        // console.log('voterContact ' + voterContactId);
-        // console.log('selectedNomination ' + this.selectedNomination);
-        // console.log('selectedContact ' + this.selectedContact);
-        console.log('this.voterEmail ' + this.voterEmail);
         createVote({finalVotes : this.posibleVotes, email: this.voterEmail})
         .then( () => {
-            // this.dispatchEvent(
-            //     new ShowToastEvent({
-            //         title: 'Success',
-            //         message: 'Your vote has been submited',
-            //         variant: 'success',
-            //     })
-            // );
             this.hasVoted = true;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Your vote has been submited',
+                    variant: 'success',
+                })
+            );
         })
         .catch(error =>{ 
+            console.log('error.body.message', error.body.message);
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Error',
@@ -104,30 +78,20 @@ export default class VotingPage extends LightningElement {
     }
 
     handleClickViewForm(evt) {
+        evt.currentTarget.style.backgroundColor = 'rgb(40, 127, 241)';
         let selectedContact = evt.currentTarget.dataset.id1;
         let nomination = evt.currentTarget.dataset.id2;
-        console.log('nomination: ' + nomination);
-        console.log('this.selectedContact ' + selectedContact);
-        // this.posibleVotes.set(nomination, selectedContact);
         this.posibleVotes[nomination] = selectedContact;
-        // this.posibleVotes = {'nomination' : nomination , 'selectedContact' : selectedContact};
-        console.log('this.posibleVotes: ', this.posibleVotes);
     }
 
-    // get nominationOptions() {
-    //     let listOfOptions = [];
-    //     this.nominations.forEach(nom => {
-    //         listOfOptions.push({ label: nom.Name, value: nom.Id });
-    //     });
-    //     return listOfOptions;
-    // }
-
-    // get campaignOptions() {
-    //     console.log('this.campaigns: ' + this.campaigns);
-    //     let listOfOptions = [];
-    //     this.campaigns.forEach(cam => {
-    //         listOfOptions.push({ label: cam.Name, value: cam.Id });
-    //     });
-    //     return listOfOptions;
-    // }
+    navigateToHome() {
+        // Use the built-in 'Navigate' method
+        this[NavigationMixin.Navigate]({
+            // Pass in pageReference
+            type: 'standard__namedPage',
+            attributes: {
+                pageName: 'home'
+            }
+        });
+    }
 }
